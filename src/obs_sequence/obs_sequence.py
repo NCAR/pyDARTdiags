f7c88bcec9392707649f0c17af1f7b78bcaf58d6
Helen Kershaw <hkershaw@ucar.edu>
2024-07-01 14:21:41 -0400

    feat: read 1d obs
    Note, I'm not sure whether there should be a timedate for the 1d obs
    
    fixes: https://github.com/NCAR/pyDARTdiags/issues/8

--- src/obs_sequence/obs_sequence.py
+++ src/obs_sequence/obs_sequence.py
@@ -24,7 +24,7 @@ class obs_sequence:
          Access the resulting pandas dataFrame:
               obs_seq.df   
 
-       latitude and longitude are in degrees in the DataFrame
+       For 3D sphere models: latitude and longitude are in degrees in the DataFrame
        sq_err = (mean-obs)**2
        bias = (mean-obs)
 
@@ -54,19 +54,25 @@ class obs_sequence:
     synonyms_for_obs = ['NCEP BUFR observation',
                         'AIRS observation', 
                         'GTSPP observation', 
-                        'SST observation']
+                        'SST observation',
+                        'observations']
     
     def __init__(self, file):
+        self.loc_mod = 'None'
         self.file = file
         self.header, self.header_n = read_header(file)
         self.types = collect_obs_types(self.header)
         self.copie_names, self.n_copies = collect_copie_names(self.header)
         self.seq = obs_reader(file, self.n_copies)
         self.all_obs = self.create_all_obs() # uses up the generator
+        # at this point you know if the seq is loc3d or loc1d
+        if self.loc_mod == 'None':
+            raise ValueError("Neither 'loc3d' nor 'loc1d' could be found in the observation sequence.")
         self.columns = self.column_headers()
         self.df = pd.DataFrame(self.all_obs, columns = self.columns)
-        self.df['longitude'] = np.rad2deg(self.df['longitude'])
-        self.df['latitude'] = np.rad2deg(self.df['latitude'])
+        if self.loc_mod == 'loc3d':
+            self.df['longitude'] = np.deg2rad(self.df['longitude'])
+            self.df['latitude'] = np.deg2rad(self.df['latitude'])
         # rename 'X observation' to observation
         self.synonyms_for_obs = [synonym.replace(' ', '_') for synonym in self.synonyms_for_obs]
         rename_dict = {old: 'observation' for old in self.synonyms_for_obs  if old in self.df.columns}
@@ -93,12 +99,22 @@ class obs_sequence:
         data = []
         data.append(obs[0].split()[1]) # obs_num
         data.extend(list(map(float,obs[1:self.n_copies+1]))) # all the copies
-        locI = obs.index('loc3d')
-        location = obs[locI+1].split()
-        data.append(float(location[0])) # location x
-        data.append(float(location[1])) # location y
-        data.append(float(location[2])) # location z
-        data.append(obs_sequence.vert[int(location[3])])
+        try:  # HK todo only have to check loc3d or loc1d for the first observation, the whole file is the same
+            locI = obs.index('loc3d')
+            location = obs[locI+1].split()
+            data.append(float(location[0]))  # location x
+            data.append(float(location[1]))  # location y
+            data.append(float(location[2]))  # location z
+            data.append(obs_sequence.vert[int(location[3])])
+            self.loc_mod = 'loc3d'
+        except ValueError:
+            try:
+                locI = obs.index('loc1d')
+                location = obs[locI+1]
+                data.append(float(location))  # 1d location 
+                self.loc_mod = 'loc1d'
+            except ValueError:
+                raise ValueError("Neither 'loc3d' nor 'loc1d' could be found in the observation sequence.")
         typeI = obs.index('kind') # type of observation
         type_value = obs[typeI + 1]
         data.append(self.types[type_value]) # observation type
@@ -106,7 +122,7 @@ class obs_sequence:
         time = obs[-2].split()
         data.append(int(time[0])) # seconds
         data.append(int(time[1])) # days
-        data.append(convert_dart_time(int(time[0]), int(time[1]))) # datetime
+        data.append(convert_dart_time(int(time[0]), int(time[1]))) # datetime   # HK todo what is approprate for 1d models?
         data.append(float(obs[-1])) # obs error variance ?convert to sd?
         
         return data
@@ -116,10 +132,13 @@ class obs_sequence:
         heading = []
         heading.append('obs_num')
         heading.extend(self.copie_names)
-        heading.append('longitude')
-        heading.append('latitude')
-        heading.append('vertical')
-        heading.append('vert_unit')
+        if self.loc_mod == 'loc3d':
+            heading.append('longitude')
+            heading.append('latitude')
+            heading.append('vertical')
+            heading.append('vert_unit')
+        elif self.loc_mod == 'loc1d':
+            heading.append('location')
         heading.append('type')
         heading.append('seconds')
         heading.append('days')
