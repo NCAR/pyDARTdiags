a3f5e5b7c9dc14c24924eff4b44e896c2859f3df
Helen Kershaw <hkershaw@ucar.edu>
2024-08-30 16:50:03 -0400

    bump version for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pydartdiags",
-    version="0.0.4",
+    version="0.0.41",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     author="Helen Kershaw",
