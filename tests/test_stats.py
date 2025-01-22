6dd44fc5827aebd772ceff8e1c68cce53581bfb4
Helen Kershaw <hkershaw@ucar.edu>
2025-01-22 13:56:49 -0500

    test: need to have posterior_ensemble_mean to check if the obs_seq has posterior info

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -9,6 +9,7 @@ class TestRankCalculation:
         data = {
             'observation': [2.5, 3.0, 4.5],  # Actual observation values
             'obs_err_var': [0.1, 0.2, 0.3],  # Variance of the observation error
+            'prior_ensemble_mean': [2.4, 3.0, 4.5], 
             'prior_ensemble_member1': [2.3, 3.1, 4.6],
             'prior_ensemble_member2': [2.4, 2.9, 4.4],
             'prior_ensemble_member3': [2.5, 3.2, 4.5],
@@ -27,9 +28,11 @@ class TestRankCalculation:
          data = {
             'observation': [2.5, 3.0, 4.5],  # Actual observation values
             'obs_err_var': [0.1, 0.2, 0.3],  # Variance of the observation error
+            'prior_ensemble_mean': [2.4, 3.0, 4.5],
             'prior_ensemble_member1': [2.3, 3.1, 4.6],
             'prior_ensemble_member2': [2.4, 2.9, 4.4],
             'prior_ensemble_member3': [2.5, 3.2, 4.5],
+            'posterior_ensemble_mean': [2.5, 3.2, 4.6],
             'posterior_ensemble_member1': [2.4, 3.2, 4.7],
             'posterior_ensemble_member2': [2.5, 3.1, 4.6],
             'posterior_ensemble_member3': [2.6, 3.3, 4.8],
