4bf088a25c5524055a81b99a6303221fa3abb781
hkershaw-brown <hkershaw-brown@users.noreply.github.com>
2025-02-06 22:45:42 +0000

    deploy: 79d879c1da781b5fe9ad1b440a15520788578539

---
+++ _downloads/3e5bbdc2e4c62182134b657f6948515a/plot_duplicates.py
@@ -0,0 +1,67 @@
+"""
+Finding Duplicates in a Observation Sequence
+============================================
+
+This example shows how to find duplicates in an observation sequence,
+and how to remove them.
+
+"""
+
+###########################################
+# Import the obs_sequence module
+import os
+import pydartdiags.obs_sequence.obs_sequence as obsq
+
+###########################################
+# Read in the observation sequence file. In this example we'll use a real obs_seq file,
+# the NCEP+ACARS.201303_6H.obs_seq2013030306 file that comes with the pyDARTdiags package.
+# This is 6 hours of observations from March 3, 2013.
+data_dir = os.path.join(os.getcwd(), "../..", "data")
+data_file = os.path.join(data_dir, "NCEP+ACARS.201303_6H.obs_seq2013030306")
+
+obs_seq = obsq.obs_sequence(data_file)
+
+###########################################
+# How many observations are in the sequence?
+num_obs = len(obs_seq.df)
+print(f"Number of observations: {num_obs}")
+
+###########################################
+# How many of each type of observation are in the sequence?
+obs_seq.df.groupby('type')['type'].count()
+
+###########################################
+# How many duplicates are there in the sequence? Lets pick the columns that we want to compare
+# to determine if an observation is a duplicate. In this case we'll use latitude, longitude, vertical,
+# time, observation, and type. We'll use the 'duplicated' method to find the duplicates.
+
+columns_to_compare = ['latitude', 'longitude', 'vertical', 'time', 'observation', 'type']
+num_dups = obs_seq.df.duplicated(subset=columns_to_compare).sum()
+print(f"Number of duplicates: {num_dups}")
+
+###########################################
+# Lets see how many duplicates there are for each type of observation.
+# We'll use the 'duplicated' method to find the duplicates.
+for obs_type in sorted(obs_seq.types.values()):
+    selected_rows = obs_seq.df[obs_seq.df['type'] == obs_type]
+    print(f"duplicates in {obs_type}: ", selected_rows[columns_to_compare].duplicated().sum())
+
+###########################################
+# Let's look at the duplicates in the 'LAND_SFC_ALTIMETER' type.
+# We're sorting by latitude to make it easier to see the duplicates.
+
+selected_rows = obs_seq.df[obs_seq.df['type'] == 'LAND_SFC_ALTIMETER']
+duplicate_mask = selected_rows[columns_to_compare].duplicated(keep=False)
+duplicate_rows = selected_rows[duplicate_mask]
+
+duplicate_rows.sort_values(by='latitude')
+
+###########################################
+# Lets remove all the duplicates from the dataFrame
+obs_seq.df.drop_duplicates(subset=columns_to_compare, inplace=True)
+
+###########################################
+# The number of obs has been reduced by the number of duplicates
+print(f"Original number of observations: {num_obs}")
+print(f"Number of duplicate observations: {num_dups}")
+print(f"Number of observations after removing duplicates: {len(obs_seq.df)}")
---
+++ _downloads/4bcf5dabc679af5a0313f35f81ea3247/plot_grand_stats.py
@@ -0,0 +1,58 @@
+"""
+Grand Statistics
+================
+
+This example demonstrates how to compute grand statistics for
+an observation sequence.
+
+"""
+
+###########################################
+# Import the obs_sequence module 
+# and the statistics module
+import pydartdiags.obs_sequence.obs_sequence as obsq
+from pydartdiags.stats import stats
+###########################################
+# Chose an obs_seq file to read.
+# This is a small obs_seq file "obs_seq.final.ascii.medium"
+# that comes with the pyDARTdiags package 
+# in the data directory, so we ``import os`` to get the path to the file
+import os
+data_dir = os.path.join(os.getcwd(), "../..", "data")
+data_file = os.path.join(data_dir, "obs_seq.final.ascii.medium")
+
+
+###########################################
+# Read the obs_seq file into an obs_seq object
+obs_seq = obsq.obs_sequence(data_file)
+
+# Select observations with QC value of 0
+qc0 = obs_seq.select_by_dart_qc(0)
+
+###########################################
+# qc0 is a dataframe with only the observations with QC value of 0.
+#
+# The columns of the dataframe are the same as the original obs_seq dataframe
+qc0.columns
+
+###########################################
+# Now we calculate the statistics required for DART diagnostics
+
+stats.diag_stats(qc0)
+
+###########################################
+# The statistics are calculated for each row in the dataframe, 
+# and the results are stored in new columns.
+
+qc0.columns
+
+###########################################
+# The help function can be used to find out more about the diag_stats function
+# including what statistics are calculated
+help(stats.diag_stats)
+
+
+###########################################
+# Summarize the grand statistics, which are the statistics aggregated over all the observations
+# for each type of observation.
+stats.grand_statistics(qc0)
