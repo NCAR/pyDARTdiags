f64aa07afbccd921ab55dbf5caa5cd99cd2c58c9
Marlee Smith <masmith@ucar.edu>
2025-07-08 14:25:10 -0600

    Adding in two new examples for the manipulation of obs seqs by selecting them on a map
    - An example to remove the obs selected on the map
    - An example to add the obs selected on the map to a new obs seq
    - Adds new source code dir interactive_plots
    - Adds dash to the requirements

---
+++ examples/04_interacting/select_to_pick_obs.py
@@ -0,0 +1,48 @@
+"""
+Select observations to add them to a new obs sequence
+=====================================================
+
+This example demonstrates read an observation sequence
+file and launch a Dash app to plot the observations on a map.
+
+You can then select observations on the map to add them to a
+new observation sequence file. Further instructions on usage
+are provided in the Dash app.
+
+"""
+
+###########################################
+# Import the obs_sequence and interactive_plots module
+from pydartdiags.interactive_plots import interactive_plots as ip
+import pydartdiags.obs_sequence.obs_sequence as obsq
+
+###########################################
+# Choose the first obs_seq file to read.
+# In this example, we use a small obs_seq file "obs_seq.final.ascii.medium"
+# that comes with the pyDARTdiags package in the data directory, so we
+# ``import os`` to get the path to the file.
+import os
+data_dir = os.path.join(os.getcwd(), "../..", "data")
+data_file = os.path.join(data_dir, "obs_seq.final.ascii.medium")
+
+###########################################
+# Read the obs_seq file into an obs_seq object.
+# We also create a second obs_seq object to be mainuplated through
+# map selection
+obs_seq = obsq.obs_sequence(data_file)
+obs_seq_selected = obsq.obs_sequence(data_file)
+
+###########################################
+# Examine the dataframe.
+obs_seq.df.head()
+
+###########################################
+# Create and run the interactive Dash app
+# that creates new obs sequence file with
+# observations selcted on geographic plot
+fig = ip.geo_plot(obs_seq)
+app = ip.include_only_selected_obs_dash_app(fig, obs_seq_selected)
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
