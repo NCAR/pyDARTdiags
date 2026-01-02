e84b00982939d9b0303288916893d5114a223c4b
Helen Kershaw <20047007+hkershaw-brown@users.noreply.github.com>
2026-01-02 14:08:08 -0500

    bump version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pydartdiags",
-    version="0.6.1",
+    version="0.6.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
